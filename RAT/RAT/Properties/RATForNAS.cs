2b21d99c3c3cd3a5f4bc310c98a861201504e275
Billy Peng <858700259@qq.com>
2017-10-11 20:03:03 +0800

    Initial commit

---
+++ RAT/RAT/Properties/RATForNAS.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+
+public class RATForNAS : PartModule
+{
+    [KSPField]
+    private Transform RotateTransform= null;
+    private float rotateAngle;
+
+    public string RotateTransformAName = "RotateTransform";
+
+    public bool IsForwardRotation = true;
+
+    public float rotateDPS = 30.0f;
+
+
+    public override void OnStart(PartModule.StartState state)
+    {
+        RotateTransform = base.part.FindModelTransform(RotateTransformAName);
+    }
+
+    void FixedUpdate()
+    {
+        if (vessel.parts.Count == 1 && vessel.Splashed == true) 
+        {
+                    rotateAngle = rotateDPS * TimeWarp.fixedDeltaTime;
+                    if (IsForwardRotation == false)
+                    {
+                        rotateAngle = rotateAngle * -1;
+                    }
+                    RotateTransform.Rotate(0, 0, rotateAngle);
+        }
+    }
+}
---
+++ RAT/RAT/Properties/RATsystem.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class RATsystem : PartModule
+{
+    [KSPField]
+    private Transform RotateTransform = null;
+    private double dynPre;
+    private double minDynPre;
+    private double generatePower;
+    private float rotateAngle;
+
+    public string resourceName = "ElectricCharge";
+    public string RotateTransformName = "RotateTransform";
+
+    public float rotateDPS = 30.0f;
+
+    public bool IsForwardRotation = true;
+
+    public double minAtmDen = 0.05;
+    public double minSpeed = 10.0;
+    public double generateKey = 1.0;
+    public double maxGenerateValue = 30.0;
+
+    public override void OnStart(PartModule.StartState state)
+    {
+        RotateTransform = base.part.FindModelTransform(RotateTransformName);
+        minDynPre = (0.5 * minAtmDen * (minSpeed * minSpeed)) / 1000;
+    }
+
+    void FixedUpdate()
+    {
+        if (1==1) //<---How hard it is???
+        {
+            dynPre = this.part.dynamicPressurekPa;
+            if (dynPre >= minDynPre) 
+            {
+                
+                generatePower = dynPre * generateKey;
+                if (generatePower > maxGenerateValue)
+                {
+                    generatePower = maxGenerateValue;
+                }
+                generatePower = generatePower * TimeWarp.fixedDeltaTime * -1;
+                this.part.RequestResource(resourceName, generatePower);
+                rotateAngle = (float)dynPre * rotateDPS * TimeWarp.fixedDeltaTime;
+                if (IsForwardRotation == false)
+                {
+                    rotateAngle = rotateAngle * -1;
+                }
+                RotateTransform.Rotate(0, 0, rotateAngle);
+            }
+        }
+    }
+}
+
